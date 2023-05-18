ddd46ff57c6ccbfa626a0bb7a9223b3ca2550f3d
Adrian Hards <github@ahards.com>
2023-05-18 14:37:40 +0100

    solve exercise one for dynamic templates

--- web_applications/03_phase/02_dynamic_templates/app.py
+++ web_applications/03_phase/02_dynamic_templates/app.py
@@ -3,8 +3,6 @@ from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
 from lib.album_repository import AlbumRepository
 from lib.album import Album
-from lib.artist_repository import ArtistRepository
-from lib.artist import Artist
 
 app = Flask(__name__)
 
@@ -17,6 +15,13 @@ def get_emoji():
 @app.route("/goodbye", methods=["GET"])
 def get_goodbye():
     return render_template("goodbye.html", goodbye="Bye!")
+
+
+@app.route("/albums")
+def get_albums():
+    connection = get_flask_database_connection(app)
+    repository = AlbumRepository(connection)
+    return render_template("albums.html", albums=repository.all())
 
 
 if __name__ == "__main__":
--- web_applications/03_phase/02_dynamic_templates/tests/test_app.py
+++ web_applications/03_phase/02_dynamic_templates/tests/test_app.py
@@ -6,18 +6,42 @@ We can get an emoji from the /emoji page
 """
 
 
-def test_get_emoji(page, test_web_address):  # Note new parameters
-    # We load a virtual browser and navigate to the /emoji page
-    page.goto(f"http://{test_web_address}/emoji")
+# def test_get_emoji(page, test_web_address):  # Note new parameters
+#     # We load a virtual browser and navigate to the /emoji page
+#     page.goto(f"http://{test_web_address}/emoji")
 
-    # We look at the <strong> tag
-    strong_tag = page.locator("strong")
+#     # We look at the <strong> tag
+#     strong_tag = page.locator("strong")
 
-    # We assert that it has the text ":)"
-    expect(strong_tag).to_have_text(":)")
+#     # We assert that it has the text ":)"
+#     expect(strong_tag).to_have_text(":)")
 
 
-def test_get_goodbye(page, test_web_address):
-    page.goto(f"http://{test_web_address}/goodbye")
-    strong_tag = page.locator("strong")
-    expect(strong_tag).to_have_text("Bye!")
+# def test_get_goodbye(page, test_web_address):
+#     page.goto(f"http://{test_web_address}/goodbye")
+#     strong_tag = page.locator("strong")
+#     expect(strong_tag).to_have_text("Bye!")
+
+
+def test_get_albums(page, test_web_address):
+    page.goto(f"http://{test_web_address}/albums")
+    titles = page.locator(".album_title")
+    years = page.locator(".album_year")
+
+    expect(titles).to_have_text(
+        [
+            "Title: Midnights",
+            "Title: reputation",
+            "Title: Going Blue",
+            "Title: folklore",
+        ]
+    )
+
+    expect(years).to_have_text(
+        [
+            "Release Date: 2022",
+            "Release Date: 2017",
+            "Release Date: 2019",
+            "Release Date: 2020",
+        ]
+    )
