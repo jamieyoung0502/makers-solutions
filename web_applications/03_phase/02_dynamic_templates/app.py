b85f097976ee61b7fad17836520b9f3ce6a4a448
Adrian Hards <github@ahards.com>
2023-05-18 16:57:08 +0100

    solve final challenge of phase 3

--- web_applications/03_phase/02_dynamic_templates/app.py
+++ web_applications/03_phase/02_dynamic_templates/app.py
@@ -8,16 +8,6 @@ from lib.album import Album
 app = Flask(__name__)
 
 
-@app.route("/emoji", methods=["GET"])
-def get_emoji():
-    return render_template("emoji.html", emoji=":)")
-
-
-@app.route("/goodbye", methods=["GET"])
-def get_goodbye():
-    return render_template("goodbye.html", goodbye="Bye!")
-
-
 @app.route("/albums")
 def get_albums():
     connection = get_flask_database_connection(app)
@@ -40,6 +30,21 @@ def get_single_album(id):
         album=album_repository.find(album_id),
         artist=artist_repository.find(album.artist_id),
     )
+
+
+@app.route("/artists/<int:id>")
+def get_single_artist(id):
+    artist_id = id
+    connection = get_flask_database_connection(app)
+    artist_repository = ArtistRepository(connection)
+    return render_template("artist.html", artist=artist_repository.find(artist_id))
+
+
+@app.route("/artists")
+def get_all_artists():
+    connection = get_flask_database_connection(app)
+    artist_repository = ArtistRepository(connection)
+    return render_template("artists.html", artists=artist_repository.all())
 
 
 if __name__ == "__main__":
--- web_applications/03_phase/02_dynamic_templates/tests/test_app.py
+++ web_applications/03_phase/02_dynamic_templates/tests/test_app.py
@@ -1,10 +1,6 @@
 from playwright.sync_api import Page, expect
 import pytest
 
-"""
-We can get an emoji from the /emoji page
-"""
-
 
 def test_get_albums(page, test_web_address):
     page.goto(f"http://{test_web_address}/albums")
@@ -49,3 +45,54 @@ def test_click_album_link(page, test_web_address):
     expect(title).to_have_text("Title: Midnights")
     expect(year).to_have_text("Release Date: 2022")
     expect(artist).to_have_text("Artist: Taylor Swift")
+
+
+"""
+When I go to /artists/1
+I see the name and genre of the given artist:
+Name: Taylor Swift
+Genre: Pop
+"""
+
+
+def test_get_single_artist(page, test_web_address):
+    page.goto(f"http://{test_web_address}/artists/1")
+    name = page.locator(".name")
+    genre = page.locator(".genre")
+    expect(name).to_have_text("Name: Taylor Swift")
+    expect(genre).to_have_text("Genre: Pop")
+
+
+"""
+When I go to /artists
+I see a list of all of the artists
+["Name: Taylor Swift", "Name: Victoria Bigelow"]
+["Genre: Pop", "Genre: Mopey"]
+"""
+
+
+def test_get_all_artists(page, test_web_address):
+    page.goto(f"http://{test_web_address}/artists")
+    names = page.locator(".name")
+    genres = page.locator(".genre")
+    expect(names).to_have_text(["Name: Taylor Swift", "Name: Victoria Bigelow"])
+    expect(genres).to_have_text(["Genre: Pop", "Genre: Mopey"])
+
+
+"""
+We go to the /artists page
+And we click on a link "Taylor Swift"
+We go to the artist's page
+I see the name and genre of the given artist:
+Name: Taylor Swift
+Genre: Pop
+"""
+
+
+def test_artist_link(page, test_web_address):
+    page.goto(f"http://{test_web_address}/artists")
+    page.click("text='Taylor Swift'")
+    name = page.locator(".name")
+    genre = page.locator(".genre")
+    expect(name).to_have_text("Name: Taylor Swift")
+    expect(genre).to_have_text("Genre: Pop")
